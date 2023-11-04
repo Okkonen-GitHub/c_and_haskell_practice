7e7fb8d0b1744fc697b6f7a2010e9e5cec1637a4
Okko <okko.contact@proton.me>
2023-11-04 12:29:31 +0200

    improve haskell #1

--- haskell_lists/app/Main.hs
+++ haskell_lists/app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 myElem :: (Eq a) => a -> [a] -> Bool
-myElem a [x] = x == a
+myElem _ [] = False
 myElem a (x:xs) = a == x || myElem a xs
 
 
@@ -10,5 +10,5 @@ main = do
   let test = [1, 2, 3, 4, 5, 6, 8, 10, 12]
   let r1 = myElem 5 test
   let r2 = myElem 7 test
-  print ("Is 5 in list: ", show r1)
-  print ("Is 5 in list: ", show r2)  
+  print ("Is 5 in list: ", r1)
+  print ("Is 7 in list: ", r2)  
