e9138de299c3c24aeeb62c8e33558978c7559c38
Okko <okko.contact@proton.me>
2024-01-04 20:28:53 +0200

    not done haskell #4

--- haskell_lists/app/Main.hs
+++ haskell_lists/app/Main.hs
@@ -41,11 +41,30 @@ test03 = unlines $ map show [r1, r2]
     r2 = isAsc [1,2,3,3,2,1]
 
 
+hasPath :: [(Int, Int)] -> Int -> Int -> [Bool]
+hasPath [] a b = []
+hasPath nodes a b = peep
+  where
+    next' = filter (\x -> a == fst x) nodes
+    next = map snd next'
+    peep = concatMap (\x -> hasPath nodes x b) next 
+    reachable_now = any (\x -> fst x == a && snd x == b) nodes
+
+test04 :: String
+test04 = unlines $ map show [r1, r3]
+  where
+    r1 = hasPath [(1,4)] 1 4 -- true
+    test = [(1,2), (2,3),(3,2),(4,3),(4,5)]
+    r2 = hasPath test 1 3 -- true
+    r3 = hasPath test 4 1 -- false
+
+
 runtest :: Int -> String
 runtest ex
   | ex == 1 = test01
   | ex == 2 = test02
   | ex == 3 = test03
+  | ex == 4 = test04
   | otherwise = undefined
 
 
