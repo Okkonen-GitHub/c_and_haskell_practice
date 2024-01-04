733e8b1d74a5106de91aae4fbfe44a3d73b46a5a
Okko <okko.contact@proton.me>
2024-01-04 19:33:25 +0200

    improve #2 and do #3

--- haskell_lists/app/Main.hs
+++ haskell_lists/app/Main.hs
@@ -16,11 +16,9 @@ test01 = do
 myNub :: (Eq a) => [a] -> [a]
 myNub [] = []
 myNub [x] = [x]
-myNub (x:xs) = 
-  if myElem x xs then
-    myNub xs
-  else
-    x : myNub xs
+myNub (x:xs) 
+  | myElem x xs = myNub xs
+  | otherwise   = x : myNub xs
 
 test02 :: String
 test02 = show res ++ "\n"
@@ -29,10 +27,25 @@ test02 = show res ++ "\n"
     res = myNub test
   
 
+isAsc :: [Int] -> Bool
+isAsc [] = True
+isAsc [x] = True
+isAsc (x:xs)
+  | x > head xs = False
+  | otherwise = isAsc xs
+
+test03 :: String
+test03 = unlines $ map show [r1, r2]
+  where
+    r1 = isAsc [1,2,3,3,4,4,5,5]
+    r2 = isAsc [1,2,3,3,2,1]
+
+
 runtest :: Int -> String
 runtest ex
   | ex == 1 = test01
   | ex == 2 = test02
+  | ex == 3 = test03
   | otherwise = undefined
 
 
