6e846fc8b27384d1a8dc0921f53b12bbd0713e07
Okko <okko.contact@proton.me>
2024-01-04 19:19:40 +0200

    fix up haskell environment and impl ex 2

--- haskell_lists/app/Main.hs
+++ haskell_lists/app/Main.hs
@@ -5,10 +5,36 @@ myElem _ [] = False
 myElem a (x:xs) = a == x || myElem a xs
 
 
-main :: IO ()
-main = do
+test01 :: String
+test01 = do
   let test = [1, 2, 3, 4, 5, 6, 8, 10, 12]
   let r1 = myElem 5 test
   let r2 = myElem 7 test
-  print ("Is 5 in list: ", r1)
-  print ("Is 7 in list: ", r2)  
+  show ("Is 5 in list: ", r1) ++ "\n" ++ show ("Is 7 in list: ", r2) ++ "\n"  
+
+
+myNub :: (Eq a) => [a] -> [a]
+myNub [] = []
+myNub [x] = [x]
+myNub (x:xs) = 
+  if myElem x xs then
+    myNub xs
+  else
+    x : myNub xs
+
+test02 :: String
+test02 = show res ++ "\n"
+  where 
+    test = [-1,0,0,1,1,2,3,4,5,5,5,5,6,6]
+    res = myNub test
+  
+
+runtest :: Int -> String
+runtest ex
+  | ex == 1 = test01
+  | ex == 2 = test02
+  | otherwise = undefined
+
+
+main :: IO ()
+main = interact $  runtest . head . map read . words
